1f727926ba29f849ba7a10ed9b404396e13a654e
Peter Buchegger <peter@pbuchegger.at>
2020-06-01 12:21:38 +0200

    cppcheck fixes for power management

--- src/power_management.cpp
+++ src/power_management.cpp
@@ -1,6 +1,11 @@
 
 #include "power_management.h"
 
+PowerManagement::PowerManagement()
+{
+}
+
+// cppcheck-suppress unusedFunction
 bool PowerManagement::begin(TwoWire port)
 {
     bool result = axp.begin(port, AXP192_SLAVE_ADDRESS);
@@ -11,32 +16,39 @@ bool PowerManagement::begin(TwoWire port)
     return result;
 }
 
+// cppcheck-suppress unusedFunction
 void PowerManagement::activateLoRa()
 {
 	axp.setPowerOutPut(AXP192_LDO2, AXP202_ON);
 }
 
+// cppcheck-suppress unusedFunction
 void PowerManagement::deactivateLoRa()
 {
 	axp.setPowerOutPut(AXP192_LDO2, AXP202_OFF);
 }
 
+// cppcheck-suppress unusedFunction
 void PowerManagement::activateGPS()
 {
 	axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);
 }
 
+// cppcheck-suppress unusedFunction
 void PowerManagement::deactivateGPS()
 {
 	axp.setPowerOutPut(AXP192_LDO3, AXP202_OFF);
 }
 
+// cppcheck-suppress unusedFunction
 void PowerManagement::activateOLED()
 {
 	axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON);
 }
 
+// cppcheck-suppress unusedFunction
 void PowerManagement::decativateOLED()
 {
 	axp.setPowerOutPut(AXP192_DCDC1, AXP202_OFF);
 }
+
--- src/power_management.h
+++ src/power_management.h
@@ -7,6 +7,7 @@
 class PowerManagement
 {
 public:
+	PowerManagement();
     bool begin(TwoWire port);
 
     void activateLoRa();
