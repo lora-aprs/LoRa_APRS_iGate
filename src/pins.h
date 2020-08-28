07d90df861c646a4f7d71a6ee0716606769a654c
Peter Buchegger <peter@pbuchegger.at>
2020-08-28 15:47:59 +0200

    add config for the heltec boards

--- src/pins.h
+++ src/pins.h
@@ -1,6 +1,24 @@
 #ifndef PINS_H_
 #define PINS_H_
 
+#ifndef DHELTEC_WIFI_LORA_32_V1
+	#undef OLED_SDA
+	#undef OLED_SCL
+	#undef OLED_RST
+	#define OLED_SDA	4
+	#define OLED_SCL	15
+	#define OLED_RST	16
+#endif
+
+#ifndef DHELTEC_WIFI_LORA_32_V2
+	#undef OLED_SDA
+	#undef OLED_SCL
+	#undef OLED_RST
+	#define OLED_SDA	4
+	#define OLED_SCL	15
+	#define OLED_RST	16
+#endif
+
 #ifdef ARDUINO_LORA32_V2
 	#undef OLED_SDA
 	#undef OLED_SCL
