cad2b5089edb46aafcbd64619398e9f8f9bb7484
Peter Buchegger <peter@pbuchegger.at>
2020-09-18 22:01:50 +0200

    fixing pins chaos and add warning as errors

--- src/display.h
+++ src/display.h
@@ -11,10 +11,4 @@ void show_display(String header, String line1, String line2, String line3, int w
 void show_display(String header, String line1, String line2, String line3, String line4, int wait = 0);
 void show_display(String header, String line1, String line2, String line3, String line4, String line5, int wait = 0);
 
-#ifdef ARDUINO_T_Beam
-#define OLED_SDA 21
-#define OLED_SCL 22
-#define OLED_RST 4
-#endif
-
 #endif
--- src/pins.h
+++ src/pins.h
@@ -1,28 +1,17 @@
 #ifndef PINS_H_
 #define PINS_H_
 
-#ifndef DHELTEC_WIFI_LORA_32_V1
-	#undef OLED_SDA
-	#undef OLED_SCL
-	#undef OLED_RST
+#undef OLED_SDA
+#undef OLED_SCL
+#undef OLED_RST
+
+#if defined(HELTEC_WIFI_LORA_32_V1) || defined(HELTEC_WIFI_LORA_32_V2) || defined(TTGO_LORA32_V1)
 	#define OLED_SDA	4
 	#define OLED_SCL	15
 	#define OLED_RST	16
 #endif
 
-#ifndef DHELTEC_WIFI_LORA_32_V2
-	#undef OLED_SDA
-	#undef OLED_SCL
-	#undef OLED_RST
-	#define OLED_SDA	4
-	#define OLED_SCL	15
-	#define OLED_RST	16
-#endif
-
-#ifdef ARDUINO_LORA32_V2
-	#undef OLED_SDA
-	#undef OLED_SCL
-	#undef OLED_RST
+#if defined(TTGO_LORA32_V2) || defined(TTGO_T_Beam_V0_7) || defined(TTGO_T_Beam_V1_0)
 	#define OLED_SDA	21
 	#define OLED_SCL	22
 	#define OLED_RST	16
