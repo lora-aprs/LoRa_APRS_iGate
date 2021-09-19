f8dfdc124626ea29720bec3e2d6c7cbe77ceabdb
Peter Buchegger <peterus@users.noreply.github.com>
2021-09-19 21:36:51 +0200

    Revert "Digipeater - Different LoRa Settings for RX and TX"

--- lib/LoRa_APRS/LoRa_APRS.h
+++ lib/LoRa_APRS/LoRa_APRS.h
@@ -22,34 +22,10 @@ public:
   void setTxFrequency(long frequency);
   long getTxFrequency() const;
 
-  void setRxSpreadingFactor(int spreadingFactor);
-  long getRxSpreadingFactor() const;
-
-  void setRxSignalBandwidth(long signalBandwidth);
-  long getRxSignalBandwidth() const;
-
-  void setRxCodingRate4(int codingRate);
-  long getRxCodingRate4() const;
-
-  void setTxSpreadingFactor(int spreadingFactor);
-  long getTxSpreadingFactor() const;
-
-  void setTxSignalBandwidth(long signalBandwidth);
-  long getTxSignalBandwidth() const;
-
-  void setTxCodingRate4(int codingRate);
-  long getTxCodingRate4() const;
-
 private:
   std::shared_ptr<APRSMessage> _LastReceivedMsg;
-  long                         _rxFrequency;
-  int                          _rxSpreadingFactor;
-  long                         _rxSignalBandwidth;
-  int                          _rxCodingRate4;
-  long                         _txFrequency;
-  int                          _txSpreadingFactor;
-  long                         _txSignalBandwidth;
-  int                          _txCodingRate4;
+  long                         _RxFrequency;
+  long                         _TxFrequency;
 };
 
 #endif
