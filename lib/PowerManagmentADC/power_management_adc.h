06df05583cc5865dbadeda52240b2bd20cafe5d4
svaio <swa@open7.de>
2021-10-25 12:20:11 +0200

    updates

--- lib/PowerManagmentADC/power_management_adc.h
+++ lib/PowerManagmentADC/power_management_adc.h
@@ -10,11 +10,11 @@ public:
   ~PowerManagementADC();
 
   bool begin(int pin, double max, double min);
-
   double getVoltage();
 
 private:
   ESP32AnalogRead adc;
+  unsigned long _updateInterval = 60000;  // In ms
   int _pin;
   double  _max_voltage;
   double  _min_voltage;
