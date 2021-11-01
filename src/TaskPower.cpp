b3aa1b7213c6acf0a39578f8b64e406788c54f35
Stefan Dietrich Wahl <stefan.wahl@campus.tu-berlin.de>
2021-11-01 01:03:55 +0100

    Info for Heap Size to find memory leaks

--- src/TaskPower.cpp
+++ src/TaskPower.cpp
@@ -2,6 +2,7 @@
 
 #include "TaskPower.h"
 #include "project_configuration.h"
+#include <TimeLib.h>
 
 PowerTask::PowerTask(TaskQueue<std::shared_ptr<APRSMessage>> &fromPower) : Task("PowerTask", 0), _fromPower(fromPower) {
 }
@@ -21,8 +22,12 @@ bool PowerTask::setup(System &system) {
 bool PowerTask::loop(System &system) {
   if (millis() > _lastTimePowerRead + _powerReadDelay) {
     _lastVoltage = _powerManagementADC.getVoltage();
+    logPrintlnI(timeString(now()) + " (UTC), ");
     logPrintI("Battery: ");
     logPrintlnI(String(_lastVoltage) + " V");
+    logPrintlnI("Free Heap         : " + String(esp_get_free_heap_size()));
+    logPrintlnI("Free Minimum Heap : " + String(esp_get_minimum_free_heap_size()));
+    logPrintlnI("Free Internal Heap: " + String(esp_get_free_internal_heap_size()));
     _stateInfo         = String(_lastVoltage) + " V";
     _lastTimePowerRead = millis();
     system.setVoltage(_lastVoltage);
