6534055ac5299bc6d7f6fa1d682dbf9f4b1aac65
Peter Buchegger <peter@pbuchegger.at>
2022-02-24 16:21:07 +0100

    format check fix

--- src/TaskRouter.h
+++ src/TaskRouter.h
@@ -2,13 +2,12 @@
 #define TASK_ROUTER_H_
 
 #include <APRSMessage.h>
-#include <TaskManager.h>
 #include <TaskMQTT.h>
+#include <TaskManager.h>
 
 class RouterTask : public Task {
 public:
-  RouterTask(TaskQueue<std::shared_ptr<APRSMessage>> &fromModem, TaskQueue<std::shared_ptr<APRSMessage>> &toModem, TaskQueue<std::shared_ptr<APRSMessage>> &toAprsIs, TaskQueue<std::shared_ptr<APRSMessage>> &toMQTT
-);
+  RouterTask(TaskQueue<std::shared_ptr<APRSMessage>> &fromModem, TaskQueue<std::shared_ptr<APRSMessage>> &toModem, TaskQueue<std::shared_ptr<APRSMessage>> &toAprsIs, TaskQueue<std::shared_ptr<APRSMessage>> &toMQTT);
   virtual ~RouterTask();
 
   virtual bool setup(System &system) override;
