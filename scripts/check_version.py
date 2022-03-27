21d3ba67e4a1d2c76aba777017c390cdc04bd466
Peter Buchegger <peter@pbuchegger.at>
2022-03-27 01:14:22 +0100

    version check update

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import git
 from datetime import date
 
 today = date.today()
@@ -16,20 +17,32 @@ with open("src/LoRa_APRS_iGate.cpp") as f:
 version_split = version.split(".")
 version_year = int(version_split[0])
 version_week = int(version_split[1])
+version_vers = int(version_split[2])
 
-print(f"firmware version year: {version_year}")
-print(f"firmware version week: {version_week}")
+print(f"[INFO] firmware version year: {version_year}")
+print(f"[INFO] firmware version week: {version_week}")
+print(f"[INFO] firmware version version: {version_vers}")
+print(f"[INFO] -> {version}")
 
-print(f"current year: {current_year}")
-print(f"current week: {current_week}")
+print(f"[INFO] current year: {current_year}")
+print(f"[INFO] current week: {current_week}")
+print(f"[INFO] -> {current_year}.{current_week}.x")
 
 error = False
 if version_year != current_year:
-    print("firmware version is not current year!")
+    print("[ERROR] firmware version is not current year!")
     error = True
 
 if version_week != current_week:
-    print("firmware version is not current week!")
+    print("[ERROR] firmware version is not current week!")
     error = True
+
+repo = git.Repo('.')
+if f"v{version}" in repo.tags:
+    print("[ERROR] tag with this version is already existing")
+    error = True
+
+if error:
+    print("[ERROR] check/update VERSION define in src/LoRa_APRS_iGate.cpp to fix this issue")
 
 exit(error)
