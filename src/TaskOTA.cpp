468cda5d57dad9d44f20c8e2f098ad248eb9a5e9
Peter Buchegger <peterus@users.noreply.github.com>
2022-06-06 23:56:26 +0200

    Update TaskOTA.cpp

--- src/TaskOTA.cpp
+++ src/TaskOTA.cpp
@@ -18,13 +18,10 @@ bool OTATask::setup(System &system) {
         } else { // U_SPIFFS
           type = "filesystem";
         }
-        system.getLogger().log(logging::LoggerLevel::LOGGER_LEVEL_INFO, getName(), "Start updating %s", type.c_str());
+        system.getLogger().log(logging::LoggerLevel::LOGGER_LEVEL_INFO, getName(), "Start updating %s. please wait, this prozess is taking some time!", type.c_str());
       })
       .onEnd([&]() {
         system.getLogger().log(logging::LoggerLevel::LOGGER_LEVEL_INFO, getName(), "OTA End");
-      })
-      .onProgress([&](unsigned int progress, unsigned int total) {
-        system.getLogger().log(logging::LoggerLevel::LOGGER_LEVEL_INFO, getName(), "Progress: %f", (progress / (total / 100)));
       })
       .onError([&](ota_error_t error) {
         String error_str;
