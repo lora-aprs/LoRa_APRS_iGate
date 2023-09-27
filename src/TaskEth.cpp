b4e7d8853b37d0a11758e5553bc5809a67a7b3fd
Peter Buchegger <peter.buchegger7@gmail.com>
2023-09-27 19:31:55 +0000

    update t internet poe

--- src/TaskEth.cpp
+++ src/TaskEth.cpp
@@ -78,27 +78,29 @@ EthTask::~EthTask() {
 bool EthTask::setup(System &system) {
   WiFi.onEvent(WiFiEvent);
 
-  // if (system.getBoardConfig()->Ethernet.nReset != -1) {
-  //   pinMode(system.getBoardConfig()->Ethernet.nReset, OUTPUT);
-  //   digitalWrite(system.getBoardConfig()->Ethernet.nReset, 0);
-  //   delay(200);
-  //   digitalWrite(system.getBoardConfig()->Ethernet.nReset, 1);
-  //   delay(200);
-  //   digitalWrite(system.getBoardConfig()->Ethernet.nReset, 0);
-  //   delay(200);
-  //   digitalWrite(system.getBoardConfig()->Ethernet.nReset, 1);
-  // }
-  //
-  // ETH.begin(system.getBoardConfig()->Ethernet.Addr, system.getBoardConfig()->Ethernet.Power, system.getBoardConfig()->Ethernet.MDC, system.getBoardConfig()->Ethernet.MDIO, system.getBoardConfig()->Ethernet.Type, system.getBoardConfig()->Ethernet.CLK);
-  //
-  // if (!system.getUserConfig()->network.DHCP) {
-  //  ETH.config(system.getUserConfig()->network.static_.ip, system.getUserConfig()->network.static_.gateway, system.getUserConfig()->network.static_.subnet, system.getUserConfig()->network.static_.dns1, system.getUserConfig()->network.static_.dns2);
-  //}
-  // if (system.getUserConfig()->network.hostname.overwrite) {
-  //  ETH.setHostname(system.getUserConfig()->network.hostname.name.c_str());
-  //} else {
-  //  ETH.setHostname(system.getUserConfig()->callsign.c_str());
-  //}
+#ifdef T_INTERNET_POE
+  if (ETH_NRESET != -1) {
+    pinMode(ETH_NRESET, OUTPUT);
+    digitalWrite(ETH_NRESET, 0);
+    delay(200);
+    digitalWrite(ETH_NRESET, 1);
+    delay(200);
+    digitalWrite(ETH_NRESET, 0);
+    delay(200);
+    digitalWrite(ETH_NRESET, 1);
+  }
+
+  ETH.begin(ETH_ADDR, ETH_POWER, ETH_MDC, ETH_MDIO, (eth_phy_type_t)ETH_TYPE, (eth_clock_mode_t)ETH_CLOCK);
+
+  if (!system.getUserConfig()->network.DHCP) {
+    ETH.config(system.getUserConfig()->network.static_.ip, system.getUserConfig()->network.static_.gateway, system.getUserConfig()->network.static_.subnet, system.getUserConfig()->network.static_.dns1, system.getUserConfig()->network.static_.dns2);
+  }
+  if (system.getUserConfig()->network.hostname.overwrite) {
+    ETH.setHostname(system.getUserConfig()->network.hostname.name.c_str());
+  } else {
+    ETH.setHostname(system.getUserConfig()->callsign.c_str());
+  }
+#endif
 
   return true;
 }
